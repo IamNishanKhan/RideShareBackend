96e38ac416a4d6bdd85d5561255bb51896d9bf30
Nishan Khan <iamnishankhan@gmail.com>
2025-03-11 00:29:54 +0600

    forgot pass, dp, update profile

--- users/serializers.py
+++ users/serializers.py
@@ -45,3 +45,26 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'gender', 'student_id', 'phone_number', 'profile_photo', 'expo_push_token', 'latitude', 'longitude']
         read_only_fields = ['email', 'student_id']
+        
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        if not User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("No account found with this email.")
+        return value
+
+class VerifyForgotPasswordOTPSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    otp_code = serializers.CharField(max_length=6)
+    new_password = serializers.CharField(write_only=True)
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is incorrect.")
+        return value
